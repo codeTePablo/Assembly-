6666b094f732c8c3e8a8b03402b19388d8be39f6
Jesus Sanchez <jesusmix8@icloud.com>
2023-05-12 17:21:46 -0600

    Push init

--- src/phase_2/main.py
+++ src/phase_2/main.py
@@ -10,136 +10,139 @@ from anlisisVariables import *
 import keyboard
 
 
-labels = []
+def main():
+    labels = []
 
+    def open_file():
+        """Abre el archivo y lo lee"""
 
-def open_file():
-    """Abre el archivo y lo lee"""
-
-    try:
-        filepath = filedialog.askopenfilename(
-            filetypes=[("ASM", "*.asm*"), ("Text Files", "*.txt")]
-        )
-        file = open(filepath, "r")
-        return file.read()
-    except Exception as e:
-        root = tk.Tk()
-        root.withdraw()
-        messagebox.showerror("Error", "Error al abrir el archivo: " + str(e))
-
-
-def clear_File(file):
-    lines = [line.strip().split(";")[0] for line in file.split("\n")]
-    lines = [line for line in lines if line]
-    clean_lines = []
-    for line in lines:
-        clean_lines.append(line)
-    return clean_lines
-
-
-def cleanLine(line):
-    line = line.replace(",", " ").rstrip().lstrip()
-    line = line.replace("  ", "").rstrip().lstrip()
-    return line
-
-
-def checkLinewithoutOperands(line, n):
-    line = line.replace(",", " ")
-    line = line.split(" ")
-    if len(line) > 1:
-        word = " ".join(line)
-        print(f"{n} - {word} :error : esta instruccion  no admite operandos")
-    else:
-        word = " ".join(line)
-        print(f"{n}- {(word)} :linea correcta")
-    pass
-
-
-def analizeCodeSegment(
-    codeSegment, tuplaNombreVariables8bits, tuplaNombresVariables16bits, n
-):
-    print(f"{n+1} - code segment :linea correcta")
-    for n, line in enumerate(codeSegment, start=n + 2):
-        if line.startswith(instrucciones_sin_operando):
-            line = cleanLine(line)
-            checkLinewithoutOperands(line, n)
-        elif line.startswith(instrucciones_con_operandos):
-            analyzeOperandsCodeSegments(
-                line, tuplaNombreVariables8bits, tuplaNombresVariables16bits, n
+        try:
+            filepath = filedialog.askopenfilename(
+                filetypes=[("ASM", "*.asm*"), ("Text Files", "*.txt")]
             )
+            file = open(filepath, "r")
+            return file.read()
+        except Exception as e:
+            root = tk.Tk()
+            root.withdraw()
+            messagebox.showerror("Error", "Error al abrir el archivo: " + str(e))
 
-        elif line.startswith(instruccionconDosOperandos):
-            analyzeTwoOperandsCodeSegments(
-                line, tuplaNombreVariables8bits, tuplaNombresVariables16bits, n
-            )
-        elif line.startswith(instrucciones_con_un_operando):
-            analyzeOneOperandCodeSegments(
-                line, tuplaNombreVariables8bits, tuplaNombresVariables16bits, n
-            )
-        elif line.startswith(instrucciondeSaltos):
-            analyceJumps(
-                line, n, labels, tuplaNombreVariables8bits, tuplaNombresVariables16bits
-            )
+    def clear_File(file):
+        lines = [line.strip().split(";")[0] for line in file.split("\n")]
+        lines = [line for line in lines if line]
+        clean_lines = []
+        for line in lines:
+            clean_lines.append(line)
+        return clean_lines
 
-        elif line.startswith(OtrasInstrucciones):
-            print(f"{n}- {line} :insturccion no asignada")
-        elif line.startswith(numbers):
-            print(f"{n}- {line} error: simbolo no definido")
-        elif line.endswith(":"):
-            if (CheckingEtiqueta(line)) == True:
-                print(f"{n} - {line} : es una etiqueta")
-                line = line.replace(":", "")
-                labels.append([line])
+    def cleanLine(line):
+        line = line.replace(",", " ").rstrip().lstrip()
+        line = line.replace("  ", "").rstrip().lstrip()
+        return line
 
-            elif (CheckingEtiqueta(line)) == False:
-                print(f"{n} - {line} parametros incorrectos")
+    def checkLinewithoutOperands(line, n):
+        line = line.replace(",", " ")
+        line = line.split(" ")
+        if len(line) > 1:
+            word = " ".join(line)
+            print(f"{n} - {word} :error : esta instruccion  no admite operandos")
         else:
-            print(f"{n}- {line} es un error")
-    print(f"{n+1} - ends :linea correcta")
+            word = " ".join(line)
+            print(f"{n}- {(word)} :linea correcta")
+        pass
 
+    def analizeCodeSegment(
+        codeSegment, tuplaNombreVariables8bits, tuplaNombresVariables16bits, n
+    ):
+        print(f"{n+1} - code segment :linea correcta")
+        for n, line in enumerate(codeSegment, start=n + 2):
+            if line.startswith(instrucciones_sin_operando):
+                line = cleanLine(line)
+                checkLinewithoutOperands(line, n)
+            elif line.startswith(instrucciones_con_operandos):
+                analyzeOperandsCodeSegments(
+                    line, tuplaNombreVariables8bits, tuplaNombresVariables16bits, n
+                )
 
-while True:
-    raw_file = open_file()
-    clean_file = clear_File(raw_file)
-    data_seccion = []
-    stack_seccion = []
-    code_seccion = []
-    seccion_actual = None
-    try:
-        for linea in clean_file:
-            if "data segment" in linea:
-                seccion_actual = data_seccion
-            elif "stack segment" in linea:
-                seccion_actual = stack_seccion
-            elif "code segment" in linea:
-                seccion_actual = code_seccion
-            elif "ends" in linea:
-                seccion_actual = None
-            elif seccion_actual is not None:
-                seccion_actual.append(linea)
-    except Exception as e:
-        print(e)
+            elif line.startswith(instruccionconDosOperandos):
+                analyzeTwoOperandsCodeSegments(
+                    line, tuplaNombreVariables8bits, tuplaNombresVariables16bits, n
+                )
+            elif line.startswith(instrucciones_con_un_operando):
+                analyzeOneOperandCodeSegments(
+                    line, tuplaNombreVariables8bits, tuplaNombresVariables16bits, n
+                )
+            elif line.startswith(instrucciondeSaltos):
+                analyceJumps(
+                    line,
+                    n,
+                    labels,
+                    tuplaNombreVariables8bits,
+                    tuplaNombresVariables16bits,
+                )
 
-    variables8bits, variables16bits, n = AnalyzerDataSegment(data_seccion)
+            elif line.startswith(OtrasInstrucciones):
+                print(f"{n}- {line} :insturccion no asignada")
+            elif line.startswith(numbers):
+                print(f"{n}- {line} error: simbolo no definido")
+            elif line.endswith(":"):
+                if (CheckingEtiqueta(line)) == True:
+                    print(f"{n} - {line} : es una etiqueta")
+                    line = line.replace(":", "")
+                    labels.append([line])
 
-    variables8BitsN, variables16BitsN = CleanVariables(variables8bits, variables16bits)
+                elif (CheckingEtiqueta(line)) == False:
+                    print(f"{n} - {line} parametros incorrectos")
+            else:
+                print(f"{n}- {line} es un error")
+        print(f"{n+1} - ends :linea correcta")
 
-    nombresVar8Bits = [variables8BitsN[0] for variables8BitsN in variables8BitsN]
+    while True:
+        raw_file = open_file()
+        clean_file = clear_File(raw_file)
+        data_seccion = []
+        stack_seccion = []
+        code_seccion = []
+        seccion_actual = None
+        try:
+            for linea in clean_file:
+                if "data segment" in linea:
+                    seccion_actual = data_seccion
+                elif "stack segment" in linea:
+                    seccion_actual = stack_seccion
+                elif "code segment" in linea:
+                    seccion_actual = code_seccion
+                elif "ends" in linea:
+                    seccion_actual = None
+                elif seccion_actual is not None:
+                    seccion_actual.append(linea)
+        except Exception as e:
+            print(e)
 
-    nombresVar16Bits = [variables16BitsN[0] for variables16BitsN in variables16BitsN]
+        variables8bits, variables16bits, n = AnalyzerDataSegment(data_seccion)
 
-    tuplaNombreVariables8bits = tuple(nombresVar8Bits)
-    tuplaNombresVariables16bits = tuple(nombresVar16Bits)
+        variables8BitsN, variables16BitsN = CleanVariables(
+            variables8bits, variables16bits
+        )
 
-    n = analyzeStackSegment(stack_seccion, n)
+        nombresVar8Bits = [variables8BitsN[0] for variables8BitsN in variables8BitsN]
 
-    analizeCodeSegment(
-        code_seccion, tuplaNombreVariables8bits, tuplaNombresVariables16bits, n
-    )
+        nombresVar16Bits = [
+            variables16BitsN[0] for variables16BitsN in variables16BitsN
+        ]
 
-    tableVariables = variables8BitsN + variables16BitsN
+        tuplaNombreVariables8bits = tuple(nombresVar8Bits)
+        tuplaNombresVariables16bits = tuple(nombresVar16Bits)
 
-    CreateTableVariables(tableVariables, labels)
-    print("Presione ESC para salir")
-    keyboard.wait("esc")
-    break
+        n = analyzeStackSegment(stack_seccion, n)
+
+        analizeCodeSegment(
+            code_seccion, tuplaNombreVariables8bits, tuplaNombresVariables16bits, n
+        )
+
+        tableVariables = variables8BitsN + variables16BitsN
+
+        CreateTableVariables(tableVariables, labels)
+        print("Presione ESC para salir")
+        keyboard.wait("esc")
+        break
